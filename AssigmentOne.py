eaff2fcd7e1703cb40cffe416429b2dbf81f9f29
Nikhil Mangal <162598548+Nikhilmangal01@users.noreply.github.com>
2026-05-29 10:13:12 +0530

    Add files via upload

---
+++ AssigmentOne.py
@@ -0,0 +1,54 @@
+# Ques. 1
+# Write a Python program to print your name, course, age and city using proper formatting. 
+
+# name = 'NIKHIL MANGAL'
+# course = 'Data Science with AI/ML'
+# age = 20
+# city = 'Jaipur'
+
+
+# print('Name:',name)
+# print('Course:',course)
+# print('Age:',age)
+# print('City:',city)
+
+# Ques. 2
+# Take user input for name and age, then print: "Hello <name>, you are <age> years old" 
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+
+# print(f'Hello {name}, you are {age} years old')
+
+
+#Ques. 3
+# Write a program to: ● Take a string input  ● Print its reverse  ● Count total number of characters 
+
+# inp = input("Enter your string: ")
+# inp_rev = inp[::-1]
+# print(f'Reverse of {inp} is: ',inp_rev)
+# print('Count: ',len(inp))
+
+# Ques. 4
+# Create a set containing duplicate numbers. 
+# Tasks: ● Print the original set ● Observe how duplicates are automatically removed ● Add a new element using add() ● Remove an element using remove()
+
+# myset = {1,2,3,4,2,6,2,35,6,1,3,4,6,1,6,423,1654,56,13,1}
+# print('Original Set: ',myset)
+# myset.add(789)
+# print('Set after adding 789: ',myset)
+# myset.remove(2)
+# print('Set after removing 2: ',myset) 
+
+# Ques. 5
+# Create a tuple of 6 numbers. Tasks: ● Print the maximum and minimum number ● Count how many times a specific number appears ● Find the index position of a given element 
+
+# mytuple = [1,256,35,478,15,96]
+# mytuple.sort()
+# print('Minimum number: ',mytuple[0])
+# print('Maximum number: ',mytuple[len(mytuple) - 1])
+# for i in mytuple:
+#     print(f'Count of {i} is: ',mytuple.count(i))
+# print('Index of 256 is: ',mytuple.index(256))
+
+
